192ed3104c53b5c3f6978e8eb519ff160448ff76
IshimweJ-Ona <j.ishimwe4@alustudent.com>
2026-05-14 18:56:22 +0200

    Updates made to allow frontend not be blocked by backend cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,6 +17,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  const port = Number(process.env.PORT ?? 5000);
+  await app.listen(port);
 }
 bootstrap();
